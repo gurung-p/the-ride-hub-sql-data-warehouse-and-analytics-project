15e0bf29fa932a09d9e780cf944ab72c73e89af6
Poonam Gurung <p.g@outlook.co.nz>
2025-04-28 19:45:03 +1200

    Update ddl_silver_layer_tables.sql

--- scripts/silver/ddl_silver_layer_tables.sql
+++ scripts/silver/ddl_silver_layer_tables.sql
@@ -12,14 +12,14 @@ Script Purpose:
 IF OBJECT_ID ('silver.crm_cust_info' , 'U') IS NOT NULL
 DROP TABLE silver.crm_cust_info;
 CREATE TABLE silver.crm_cust_info (
-	cst_id				      INT,
-	cst_key				      NVARCHAR(50),
-	cst_firstname		    NVARCHAR(50),
-	cst_lastname		    NVARCHAR(50),
+	cst_id			INT,
+	cst_key			NVARCHAR(50),
+	cst_firstname		NVARCHAR(50),
+	cst_lastname		NVARCHAR(50),
 	cst_marital_status	NVARCHAR(50),
-	cst_gndr			      NVARCHAR(50),
-	cst_create_date		  DATE,
-	dwh_create_date     DATETIME2 DEFAULT GETDATE() -- ADDED FOR METADATA
+	cst_gndr		NVARCHAR(50),
+	cst_create_date		DATE,
+	dwh_create_date    	DATETIME2 DEFAULT GETDATE() -- ADDED FOR METADATA
 );
 
 IF OBJECT_ID ('silver.crm_prd_info' , 'U') IS NOT NULL
