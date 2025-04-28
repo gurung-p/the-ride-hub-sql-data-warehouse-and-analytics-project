f31202dda93b1cd595b3ba14496b9dca65f8e3f5
Poonam Gurung <p.g@outlook.co.nz>
2025-04-28 19:52:54 +1200

    Update and rename ddl.bronze.sql to ddl_bronze_layer_tables.sql

--- scripts/bronze/ddl_bronze_layer_tables.sql
+++ scripts/bronze/ddl_bronze_layer_tables.sql
@@ -14,12 +14,12 @@ Run this script to re-define the DDL structure of the 'bronze' Tables
 IF OBJECT_ID ('bronze.crm_cust_info' , 'U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info (
-	cst_id				INT,
-	cst_key				NVARCHAR(50),
+	cst_id			INT,
+	cst_key			NVARCHAR(50),
 	cst_firstname		NVARCHAR(50),
 	cst_lastname		NVARCHAR(50),
 	cst_marital_status	NVARCHAR(50),
-	cst_gndr			NVARCHAR(50),
+	cst_gndr		NVARCHAR(50),
 	cst_create_date		DATE
 );
 
@@ -31,7 +31,7 @@ CREATE TABLE bronze.crm_prd_info (
 	prd_nm			NVARCHAR(50),
 	prd_cost		INT,
 	prd_line		NVARCHAR(50),
-	prd_start_dt	DATETIME,
+	prd_start_dt		DATETIME,
 	prd_end_dt		DATETIME
 );
 
@@ -41,11 +41,11 @@ CREATE TABLE bronze.crm_sales_details (
 	sls_ord_num		NVARCHAR(50),
 	sls_prd_key		NVARCHAR(50),
 	sls_cust_id		INT,
-	sls_order_dt	INT,
+	sls_order_dt		INT,
 	sls_ship_dt		INT,
 	sls_due_dt		INT,
 	sls_sales		INT,
-	sls_quantity	INT,
+	sls_quantity		INT,
 	sls_price		INT
 );
 
@@ -67,8 +67,8 @@ CREATE TABLE bronze.crm_sales_details (
  IF OBJECT_ID ('bronze.erp_px_cat_g1v2' , 'U') IS NOT NULL
 	DROP TABLE bronze.erp_px_cat_g1v2;
  CREATE TABLE bronze.erp_px_cat_g1v2 (
-	 id				NVARCHAR(50),
-	 cat			NVARCHAR(50),
-	 subcat			NVARCHAR(50),
+	 id		NVARCHAR(50),
+	 cat		NVARCHAR(50),
+	 subcat		NVARCHAR(50),
 	 maintenance	NVARCHAR(50)
  );
