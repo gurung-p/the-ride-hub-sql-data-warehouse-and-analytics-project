0015d47a7002ce11a34c9fda71c8c9f30987a8c5
Poonam Gurung <p.g@outlook.co.nz>
2025-06-12 21:29:22 +1200

    Update create_customers_dimension_view_gold_layer.sql

--- scripts/gold_layer/create_customers_dimension_view_gold_layer.sql
+++ scripts/gold_layer/create_customers_dimension_view_gold_layer.sql
@@ -22,15 +22,15 @@ exploration smooth and efficient
 
 SELECT 
 	ci.cst_id,
-    ci.cst_key,
-    ci.cst_firstname,
-    ci.cst_lastname,
-    ci.cst_marital_status,
-    ci.cst_gndr,
-    ci.cst_create_date,
-	ca.bdate,
-	ca.gen,
-	cl.cntry
+    	ci.cst_key,
+    	ci.cst_firstname,
+    	ci.cst_lastname,
+    	ci.cst_marital_status,
+    	ci.cst_gndr,
+    	ci.cst_create_date,
+    	ca.bdate,
+    	ca.gen,
+    	cl.cntry
  FROM silver.crm_cust_info AS ci
  LEFT JOIN silver.erp_cust_az12 AS ca
  ON ci.cst_key = ca.cid
@@ -40,24 +40,24 @@ SELECT
 
  -- POST JOIN - Check for any duplicates
  SELECT cst_id, count (*) FROM (
-	 SELECT 
-		ci.cst_id,
-		ci.cst_key,
-		ci.cst_firstname,
-		ci.cst_lastname,
-		ci.cst_marital_status,
-		ci.cst_gndr,
-		ci.cst_create_date,
-		ca.bdate,
-		ca.gen,
-		cl.cntry
-	 FROM silver.crm_cust_info AS ci
-	 LEFT JOIN silver.erp_cust_az12 AS ca
-	 ON ci.cst_key = ca.cid
-	 LEFT JOIN silver.erp_loc_a101 AS cl
-	 ON ci.cst_key = cl.cid
- )t GROUP BY cst_id
- HAVING COUNT(*) > 1
+ SELECT 
+	ci.cst_id,
+	ci.cst_key,
+	ci.cst_firstname,
+	ci.cst_lastname,
+	ci.cst_marital_status,
+	ci.cst_gndr,
+	ci.cst_create_date,
+	ca.bdate,
+	ca.gen,
+	cl.cntry
+FROM silver.crm_cust_info AS ci
+	LEFT JOIN silver.erp_cust_az12 AS ca
+	ON ci.cst_key = ca.cid
+	LEFT JOIN silver.erp_loc_a101 AS cl
+	ON ci.cst_key = cl.cid
+)t GROUP BY cst_id
+HAVING COUNT(*) > 1
 
   -- Data Integration
 
@@ -90,16 +90,16 @@ ORDER BY 1, 2
 
 SELECT 
 	ci.cst_id AS customer_id,
-    ci.cst_key AS customer_number,
-    ci.cst_firstname AS first_name,
-    ci.cst_lastname AS last_name,
+    	ci.cst_key AS customer_number,
+	ci.cst_firstname AS first_name,
+    	ci.cst_lastname AS last_name,
 	cl.cntry AS country,
-    ci.cst_marital_status AS martial_status,
+    	ci.cst_marital_status AS martial_status,
 	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the master source for gender
 	ELSE COALESCE(ca.gen, 'n/a')
 	END AS gender,
 	ca.bdate AS birth_date,
-    ci.cst_create_date AS create_date
+    	ci.cst_create_date AS create_date
  FROM silver.crm_cust_info AS ci
  LEFT JOIN silver.erp_cust_az12 AS ca
  ON ci.cst_key = ca.cid
@@ -111,49 +111,49 @@ SELECT
 SELECT 
 	ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
 	ci.cst_id AS customer_id,
-    ci.cst_key AS customer_number,
-    ci.cst_firstname AS first_name,
-    ci.cst_lastname AS last_name,
+   	ci.cst_key AS customer_number,
+	ci.cst_firstname AS first_name,
+   	ci.cst_lastname AS last_name,
 	cl.cntry AS country,
-    ci.cst_marital_status AS martial_status,
+    	ci.cst_marital_status AS martial_status,
 	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the master source for gender
 	ELSE COALESCE(ca.gen, 'n/a')
 	END AS gender,
 	ca.bdate AS birth_date,
-    ci.cst_create_date AS create_date
- FROM silver.crm_cust_info AS ci
- LEFT JOIN silver.erp_cust_az12 AS ca
- ON ci.cst_key = ca.cid
- LEFT JOIN silver.erp_loc_a101 AS cl
- ON ci.cst_key = cl.cid
+    	ci.cst_create_date AS create_date
+FROM silver.crm_cust_info AS ci
+LEFT JOIN silver.erp_cust_az12 AS ca
+ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 AS cl
+ON ci.cst_key = cl.cid
 
 
 -- =============================================================================
 -- Create Dimension: gold.dim_customers
 -- =============================================================================
 
- CREATE VIEW gold.dim_customers AS
- SELECT 
-	ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
+CREATE VIEW gold.dim_customers AS
+SELECT 
+ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
 	ci.cst_id AS customer_id,
-    ci.cst_key AS customer_number,
-    ci.cst_firstname AS first_name,
-    ci.cst_lastname AS last_name,
+	ci.cst_key AS customer_number,
+	ci.cst_firstname AS first_name,
+    	ci.cst_lastname AS last_name,
 	cl.cntry AS country,
-    ci.cst_marital_status AS martial_status,
+    	ci.cst_marital_status AS martial_status,
 	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the master source for gender
 	ELSE COALESCE(ca.gen, 'n/a')
 	END AS gender,
 	ca.bdate AS birth_date,
-    ci.cst_create_date AS create_date
- FROM silver.crm_cust_info AS ci
- LEFT JOIN silver.erp_cust_az12 AS ca
- ON ci.cst_key = ca.cid
- LEFT JOIN silver.erp_loc_a101 AS cl
- ON ci.cst_key = cl.cid
+    	ci.cst_create_date AS create_date
+FROM silver.crm_cust_info AS ci
+LEFT JOIN silver.erp_cust_az12 AS ca
+ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 AS cl
+ON ci.cst_key = cl.cid
 
- -- POST VIEW CREATION - Quality Check
+-- POST VIEW CREATION - Quality Check
 
- SELECT * FROM gold.dim_customers
+SELECT * FROM gold.dim_customers
 
- SELECT DISTINCT gender FROM gold.dim_customers
+SELECT DISTINCT gender FROM gold.dim_customers
