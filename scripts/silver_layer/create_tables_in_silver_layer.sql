55435216a7b690224205a8698e40cf634640de65
Poonam Gurung <p.g@outlook.co.nz>
2025-06-12 21:37:59 +1200

    Update create_tables_in_silver_layer.sql

--- scripts/silver_layer/create_tables_in_silver_layer.sql
+++ scripts/silver_layer/create_tables_in_silver_layer.sql
@@ -13,7 +13,8 @@ Run this script to re-define the DDL structure of the 'Silver' Tables
 
 
 IF OBJECT_ID ('silver.crm_cust_info' , 'U') IS NOT NULL
-	DROP TABLE silver.crm_cust_info;
+DROP TABLE silver.crm_cust_info;
+
 CREATE TABLE silver.crm_cust_info (
 	cst_id				INT,
 	cst_key				NVARCHAR(50),
@@ -26,7 +27,8 @@ CREATE TABLE silver.crm_cust_info (
 );
 
 IF OBJECT_ID ('silver.crm_prd_info' , 'U') IS NOT NULL
-	DROP TABLE silver.crm_prd_info;
+DROP TABLE silver.crm_prd_info;
+
 CREATE TABLE silver.crm_prd_info (
 	prd_id			INT,
 	cat_id			NVARCHAR(50),
@@ -40,7 +42,8 @@ CREATE TABLE silver.crm_prd_info (
 );
 
 IF OBJECT_ID ('silver.crm_sales_details' , 'U') IS NOT NULL
-	DROP TABLE silver.crm_sales_details;
+DROP TABLE silver.crm_sales_details;
+
 CREATE TABLE silver.crm_sales_details (
 	sls_ord_num		NVARCHAR(50),
 	sls_prd_key		NVARCHAR(50),
@@ -54,32 +57,33 @@ CREATE TABLE silver.crm_sales_details (
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
- IF OBJECT_ID ('silver.erp_loc_a101' , 'U') IS NOT NULL
-	DROP TABLE silver.erp_loc_a101;
- CREATE TABLE silver.erp_loc_a101 (
+IF OBJECT_ID ('silver.erp_loc_a101' , 'U') IS NOT NULL
+DROP TABLE silver.erp_loc_a101;
+
+CREATE TABLE silver.erp_loc_a101 (
 	 cid	NVARCHAR(50),
 	 cntry	NVARCHAR(50),
 	 dwh_create_date DATETIME2 DEFAULT GETDATE()
  );
 
- IF OBJECT_ID ('silver.erp_cust_az12' , 'U') IS NOT NULL
-	DROP TABLE silver.erp_cust_az12;
- CREATE TABLE silver.erp_cust_az12 (
+IF OBJECT_ID ('silver.erp_cust_az12' , 'U') IS NOT NULL
+DROP TABLE silver.erp_cust_az12;
+
+CREATE TABLE silver.erp_cust_az12 (
 	 cid	NVARCHAR(50),
 	 bdate	DATE,
 	 gen	NVARCHAR(50),
 	 dwh_create_date DATETIME2 DEFAULT GETDATE()
  );
 
- IF OBJECT_ID ('silver.erp_px_cat_g1v2' , 'U') IS NOT NULL
-	DROP TABLE silver.erp_px_cat_g1v2;
- CREATE TABLE silver.erp_px_cat_g1v2 (
+IF OBJECT_ID ('silver.erp_px_cat_g1v2' , 'U') IS NOT NULL
+DROP TABLE silver.erp_px_cat_g1v2;
+
+CREATE TABLE silver.erp_px_cat_g1v2 (
 	 id				NVARCHAR(50),
 	 cat			NVARCHAR(50),
 	 subcat			NVARCHAR(50),
 	 maintenance	NVARCHAR(50),
 	 dwh_create_date DATETIME2 DEFAULT GETDATE()
  );
-
-
 
